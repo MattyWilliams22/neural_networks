67c1152517b2048b129ff5308ab6761c11d2c004
Kritik Pant <kp1222@ic.ac.uk>
2024-11-19 07:43:42 +0000

    Initial pytorch model code

--- part2_house_value_regression.py
+++ part2_house_value_regression.py
@@ -3,6 +3,9 @@ import pickle
 import numpy as np
 import pandas as pd
 
+CATEGORICAL_COLUMNS = ['ocean_proximity']
+TARGET_COLUMN = 'median_house_value'
+
 class Regressor():
 
     def __init__(self, x, nb_epoch = 1000):
@@ -28,18 +31,26 @@ class Regressor():
         self.input_size = X.shape[1]
         self.output_size = 1
         self.nb_epoch = nb_epoch 
+
+        self.test_data = None
+        self.test_target = None
+
+        self.target_mean = 0
+        self.target_std = 1
+
+        self.model = None
         return
 
         #######################################################################
         #                       ** END OF YOUR CODE **
         #######################################################################
 
-    def _preprocessor(self, x, y = None, training = False):
+    def _preprocessor(self, df, y = None, training = False):
         """ 
         Preprocess input of the network.
           
         Arguments:
-            - x {pd.DataFrame} -- Raw input array of shape 
+            - df {pd.DataFrame} -- Raw input array of shape 
                 (batch_size, input_size).
             - y {pd.DataFrame} -- Raw target array of shape (batch_size, 1).
             - training {boolean} -- Boolean indicating if we are training or 
@@ -59,8 +70,45 @@ class Regressor():
 
         # Replace this code with your own
         # Return preprocessed x and y, return None for y if it was None
-        return x, (y if isinstance(y, pd.DataFrame) else None)
+        
 
+        for col in df.columns.difference(CATEGORICAL_COLUMNS):
+            df[col] = df[col].fillna(df[col].mean())
+
+        df = pd.get_dummies(df, columns=CATEGORICAL_COLUMNS)
+
+        # convert to numpy array
+        data = df.to_numpy().astype(np.float32)
+        if y:
+            target = y.to_numpy().astype(np.float32)
+            # normalize target
+            self.target_mean = target.mean()
+            self.target_std = target.std()
+            target = (target - self.target_mean) / self.target_std
+
+        # normalize data
+        data_mean = data.mean(axis=0)
+        data_std = data.std(axis=0)
+        data = (data - data_mean) / data_std
+
+        if training:
+        # split into train and test
+            np.random.seed(42)
+            indices = np.random.permutation(data.shape[0])
+            train_indices = indices[:int(0.8 * data.shape[0])]
+            test_indices = indices[int(0.8 * data.shape[0]):]
+
+            train_data = data[train_indices]
+            if y:
+                train_target = target[train_indices]
+                self.test_target = target[test_indices]
+
+            self.test_data = data[test_indices]
+
+            return torch.tensor(train_data), torch.tensor(train_target).unsqueeze(1)
+
+        else:
+            return torch.tensor(data), None
         #######################################################################
         #                       ** END OF YOUR CODE **
         #######################################################################
@@ -84,7 +132,42 @@ class Regressor():
         #                       ** START OF YOUR CODE **
         #######################################################################
 
-        X, Y = self._preprocessor(x, y = y, training = True) # Do not forget
+        X, Y = self._preprocessor(x, y = y, training = True) # Do not forget4
+
+        model = torch.nn.Sequential(
+            torch.nn.Linear(self.input_size, 64),
+            torch.nn.ReLU(),
+            torch.nn.Linear(64, 32),
+            torch.nn.ReLU(),
+            torch.nn.Linear(32, 16),
+            torch.nn.ReLU(),
+            torch.nn.Linear(16, self.output_size)
+        )
+
+        loss_fn = torch.nn.MSELoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+        for epoch in range(self.nb_epoch):
+            # Forward pass
+            y_pred = model(X)
+
+            # Compute loss
+            loss = loss_fn(y_pred, Y)
+
+            # Zero gradients
+            optimizer.zero_grad()
+
+            # Backward pass
+            loss.backward()
+
+            # Optimize
+            optimizer.step()
+
+            if epoch % 100 == 0:
+                print(f'Epoch {epoch}, loss {loss.item()}')
+
+        self.model = model
+
         return self
 
         #######################################################################
@@ -110,7 +193,13 @@ class Regressor():
         #######################################################################
 
         X, _ = self._preprocessor(x, training = False) # Do not forget
-        pass
+        
+        if self.model is None:
+            raise Exception("Model not trained")
+        
+        with torch.no_grad():
+            return self.model(X).numpy() * self.target_std + self.target_mean
+        
 
         #######################################################################
         #                       ** END OF YOUR CODE **
@@ -135,7 +224,13 @@ class Regressor():
         #######################################################################
 
         X, Y = self._preprocessor(x, y = y, training = False) # Do not forget
-        return 0 # Replace this code with your own
+        
+        if self.model is None:
+            raise Exception("Model not trained")
+        
+        with torch.no_grad():
+            Y_pred = self.model(X).numpy() * self.target_std + self.target_mean
+            return np.mean((Y_pred - self.test_target) ** 2)
 
         #######################################################################
         #                       ** END OF YOUR CODE **
@@ -191,8 +286,7 @@ def perform_hyperparameter_search():
 
 
 def example_main():
-
-    output_label = "median_house_value"
+    output_label = TARGET_COLUMN
 
     # Use pandas to read CSV data as it contains various object types
     # Feel free to use another CSV reader tool
